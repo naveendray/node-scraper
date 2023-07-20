a2a3c1d6333efac502558cdb14e2898cbe8f323e
Yasitha Herath <yasitha@Yasithas-MacBook-Pro.local>
2023-07-20 14:58:40 +0530

    modify config to add multiple configurations

--- config.js
+++ config.js
@@ -1,5 +1,13 @@
+// module.exports = {
+//     folderpath: './data/Arecanut/',
+//     spice: "Arecanut",
+//     tableIndex: 8,
+//     tableColumns: [0, 1, 2]
+// }
+
 module.exports = {
-    folderpath: './data/Arecanut/',
-    spice: "Arecanut",
-    tableIndex: 8
+    folderpath: './data/Pepper/',
+    spice: "Pepper",
+    tableIndex: 0,
+    tableColumns: [0, 1, 3]
 }
